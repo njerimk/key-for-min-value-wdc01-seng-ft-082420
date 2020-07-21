ab9bcef965103ab92f3491c8676d86697871f3e5
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-07-21 13:43:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,6 @@
 def key_for_min_value(name_hash)
 hash_1 = {:blake => 500, :ashley => 2, :adam => 1}
 hash_2 = {:blake => 10, :ashley => 50, :adam => 17}
-empty_hash = {}
 min_value = 600
 name = ""
   name_hash.each do |key, value|
